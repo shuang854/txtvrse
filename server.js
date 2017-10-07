0d4552279c166f9d16e6cd2bd2f3ce179b328797
Conflagrationator <willmarrujo@gmail.com>
2017-10-07 14:44:54 -0400

    prettifier stuff

--- server.js
+++ server.js
@@ -32,21 +32,35 @@ io.on("connection", function(socket) {
     socket.on("command", function(data) {
         var res = game.perform(data, world);
         if (res.scope == "global")
-            io.emit("response", {"res": res});
+            io.emit("response", {
+                "res": res
+            });
         else
-            socket.emit("response", {"res": res});
-        
-        var surroundingRoom = world.rooms.filter((r) => {return res.room == r.id})[0];
+            socket.emit("response", {
+                "res": res
+            });
+
+        var surroundingRoom = world.rooms.filter((r) => {
+            return res.room == r.id
+        })[0];
         res.playersInRoom.forEach((p) => {
-            io.emit("surroundings", {"enemy": p, "room": surroundingRoom, "socket": res.playerId});
+            io.emit("surroundings", {
+                "enemy": p,
+                "room": surroundingRoom,
+                "socket": res.playerId
+            });
         });
     });
 
     socket.on("register", function(name) {
         world.addPlayer(name, socket.id)
-        socket.emit("id", {"socket": socket.id})
+        socket.emit("id", {
+            "socket": socket.id
+        })
         console.log("-> player " + name + " has joined the server")
-        socket.emit("playerStatus", {"room": world.getPlayerByName(name).room})
+        socket.emit("playerStatus", {
+            "room": world.getPlayerByName(name).room
+        })
     });
 
 });
